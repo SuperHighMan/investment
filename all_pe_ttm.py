7c497c6acd93b12412fcdafdb17886cae98fb08d
SuperHighMan <465475547@qq.com>
2018-04-13 19:59:19 +0800

    all_pe_tm

--- all_pe_ttm.py
+++ all_pe_ttm.py
@@ -10,9 +10,29 @@ import pandas as pd
 
 STOCK_LIST = '/home/chenhui/investment/data/my_stock/stock_china.xlsx'
 
-sheet = pd.read_excel(STOCK_LIST, converters={u'代码':str})
-#investment.save_stock_pe_ttm(sheet[u'代码'], ktype='W', start='2008-01-01', end=None)
 
-investment.save_stock_pb(sheet[u'代码'], ktype='W', start='2008-01-01', end=None)
 
-print('我的股票池计算完毕')
+def get_all_pb():
+    """
+    计算所有股票的历史市净率
+    :return:
+    """
+    sheet = pd.read_excel(STOCK_LIST, converters={u'代码':str})
+    #investment.save_stock_pe_ttm(sheet[u'代码'], ktype='W', start='2008-01-01', end=None)
+
+    investment.save_stock_pb(sheet[u'代码'], ktype='W', start='2008-01-01', end=None)
+
+    print('我的股票池计算完毕')
+
+def get_all_pe():
+    """
+    计算所有股票的滚动市盈率
+    :return:
+    """
+    sheet = pd.read_excel(STOCK_LIST, converters={u'代码':str})
+    investment.save_stock_pe_ttm(sheet[u'代码'], ktype='W', start='2008-01-01', end=None)
+    print('我的股票池计算完毕')
+
+
+if __name__ == '__main__':
+    get_all_pe()
